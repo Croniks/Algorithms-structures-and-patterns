dc528951ca9f0c12dd42fc26cacc1a49797b3c72
Kroneks <wowmandarin@yandex.ru>
2018-12-27 07:13:36 +0300

    v.10

---
+++ TestSquareEveryDigit/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+
+namespace TestSquareEveryDigit
+{
+    class Programm
+    {
+        public static int SquareDigits(int n)
+        {
+            int result = 0;
+            int digit = 0;
+            
+            while (n > 0)
+            {
+                int value = n % 10;
+
+                if (value != 0)
+                {
+                    result += (int)Math.Pow(value, 2) * (int)Math.Pow(10, digit);
+                    
+                    if(value > 4)
+                        digit += 2;
+                    else
+                        digit++;
+                }
+                else
+                    digit++;
+                
+                n /= 10;
+            }
+
+            return result;
+        }
+            
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(SquareDigits(9119));
+            Console.WriteLine(SquareDigits(9119));
+            Console.WriteLine(SquareDigits(123));
+            Console.WriteLine(SquareDigits(5972));
+            Console.WriteLine(SquareDigits(2106));
+            Console.WriteLine(SquareDigits(2006));
+            Console.WriteLine(SquareDigits(0030502));
+            Console.WriteLine(SquareDigits(6));
+            
+            Console.ReadKey();
+        }
+    }
+}
